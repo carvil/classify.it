660508df95940694a9230a7206d64ec8a8697d93
Carlos Vilhena <carlosvilhena@gmail.com>
2012-04-05 11:17:24 +0100

    add classifier to application; converted into sinatra base app

--- application.rb
+++ application.rb
@@ -1,15 +1,32 @@
-require 'sinatra'
+require 'sinatra/base'
 require './lib/page'
+require './lib/frequency'
+require './lib/classifier'
+require './lib/techdata'
 
-get '/' do
-  haml :home
-end
+class TechClassifier < Sinatra::Base
 
-post '/' do
-  site = params[:site]
-  if content = Page.fetch_page(site)
-    haml :home, locals: {result: "YES", site: site}
-  else
-    haml :home, locals: {result: "NO", site: site}
+  # Include the keywords
+  include TechData
+
+  # Initialize the classifier
+  set :classifier, Classifier.new
+
+  # Main page
+  get '/' do
+    haml :home
   end
+
+  # Classify a url
+  post '/' do
+    site = params[:site]
+    if content = Page.fetch_page(site)
+      frequency = Frequency.count_frequency(content, keywords)
+      result = options.classifier.classify(frequency)
+      haml :home, locals: {result: result, site: site}
+    else
+      haml :home, locals: {result: result, site: site}
+    end
+  end
+
 end
