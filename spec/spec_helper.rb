4eeffa92a51c2100a2508180b4f98d59b2277e5a
Carlos Vilhena <carlosvilhena@gmail.com>
2012-04-05 13:42:29 +0100

    delete unnecessary rspec config options

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,11 +11,7 @@ require File.join(File.dirname(__FILE__), '..', 'lib', 'frequency')
 require File.join(File.dirname(__FILE__), '..', 'lib', 'classifier')
 require File.join(File.dirname(__FILE__), '..', 'lib', 'techdata')
 
-
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
   config.extend VCR::RSpec::Macros
 end
 
