02c9562cc06f7bedbc3791b906277bd3b402e3c4
Carlos Vilhena <carlosvilhena@gmail.com>
2012-04-05 11:15:12 +0100

    refactor keywords

--- lib/techdata.rb
+++ lib/techdata.rb
@@ -2,17 +2,17 @@ module TechData
 
   def keywords
     [
-      "technology", "tech", "programming", "startup",
-      "hack", "code", "scripting", "github", "ycombinator", 
-      "iphone", "android", "hacker",
+      "tech", "programming", "startup",
+      "hack", "scripting", "github", "ycombinator", 
+      "android", "hacker", "coderwall",
       "startups", "technologies", "techcrunch",
       "computing", "cloud", "ventures", "apple", "microsoft", 
       "linux","virtualization", "funding", "gadgets", "hackathon",
       "tablet", "software", "hardware", "ruby", "node.js",
       "java", "haskell", "scala", "clojure", "functional",
       "wired", "ycombinator", "ipad", "ipod","coffeescript",
-      "app", "ios", "developer", "developers", "crowdfunding",
-      "daringfireball", "apps", "heroku", "linode", "aws",
+      "ios", "developer", "developers", "crowdfunding",
+      "daringfireball", "heroku", "linode", "aws",
       "markdown", "postgres", "mysql", "riak", "nosql", "sql"
     ]
   end
