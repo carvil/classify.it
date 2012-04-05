d60d69ce7dd7dd9844b5f7bf2c4c7767614207a9
Carlos Vilhena <carlosvilhena@gmail.com>
2012-04-05 10:49:18 +0100

    module with basic data

---
+++ lib/techdata.rb
@@ -0,0 +1,20 @@
+module TechData
+
+  def keywords
+    [
+      "technology", "tech", "programming", "startup",
+      "hack", "code", "scripting", "github", "ycombinator", 
+      "iphone", "android", "hacker",
+      "startups", "technologies", "techcrunch",
+      "computing", "cloud", "ventures", "apple", "microsoft", 
+      "linux","virtualization", "funding", "gadgets", "hackathon",
+      "tablet", "software", "hardware", "ruby", "node.js",
+      "java", "haskell", "scala", "clojure", "functional",
+      "wired", "ycombinator", "ipad", "ipod","coffeescript",
+      "app", "ios", "developer", "developers", "crowdfunding",
+      "daringfireball", "apps", "heroku", "linode", "aws",
+      "markdown", "postgres", "mysql", "riak", "nosql", "sql"
+    ]
+  end
+
+end
